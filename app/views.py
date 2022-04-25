b7cf856fe5e6cd7398b62b77242e93972293dde6
AntonyBazin <antonyvbazin@gmail.com>
2022-04-26 00:50:41 +0300

    Server side operational

--- app/views.py
+++ app/views.py
@@ -2,17 +2,26 @@ from aiohttp import web
 from app.db import requests
 from datetime import datetime
 import uuid
+import json
+
+
+def get_layers(record, counter):
+    if counter > 0:
+        return {'entity': get_layers(record, counter - 1)}
+    return {'entity': ''}
 
 
 async def handler(request):
     uuidv4 = uuid.uuid4()
-    if request.rel_url.query:
-        attachment_depth = request.rel_url.query['attachment_depth']
+    dtime = datetime.today().isoformat()
+    attachment_depth = request.rel_url.query['attachment_depth'] if request.rel_url.query else '0'
+    attachment_depth = int(attachment_depth) if attachment_depth.isdigit() else 0
+    attachment = get_layers({'entity': ''}, attachment_depth)
+
     async with request.app['db'].acquire() as conn:
         cursor = await conn.execute(requests.insert().values({'request_uuid': uuidv4,
-                                                              'request_date': datetime.today().isoformat(),
-                                                              'attachment': {'entity': {'entity': {}}}}))
+                                                              'request_date': dtime,
+                                                              'attachment': attachment}))
         inserted_id = await cursor.fetchone()
-        a = await conn.execute(requests.select().where(requests.c.id == inserted_id[0]))
-        data = await a.fetchone()
-        return web.Response(text=str(data))
+        data = dict(zip(('request_uuid', 'request_date', 'attachment'), map(str, (uuidv4, dtime, attachment))))
+        return web.json_response(data)
