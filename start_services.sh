37486e1da0b2e948aa20b3f6c5b5726426cdd38b
AntonyBazin <antonyvbazin@gmail.com>
2022-04-28 12:30:10 +0300

    Fixed port collision issue

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,4 @@
 
 python init_db.py
+python app/mosk.py
 python runner.py
--- wait_for_postgres.sh
+++ wait_for_postgres.sh
@@ -4,9 +4,9 @@ host="$1"
 shift
 
 until PGPASSWORD='req_passwd' psql -h "$host" -d "requeststask" -U "req_user" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
+  >&2 echo "Postgres database is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
+>&2 echo "Postgres database is up - proceeding"
 exec "$@"
